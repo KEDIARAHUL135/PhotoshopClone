e151eae4ce4cc77e2b7d3a55a10c7abd2fdf8b19
KEDIARAHUL135 <kediarahul135@gmail.com>
2021-05-27 21:35:04 +0530

    Refactor code
    Used selected region class for polygon lasso tool

--- lasso.py
+++ lasso.py
@@ -66,128 +66,80 @@ def LassoTool(Canvas, window_title):
 
 ########################################################### Polygon Lasso Tool ########################################################################
 
-def CallBackFunc_PolyLassoTool(event, x, y, flags, params):
-    # Taking global params
-    global selecting, isSelected, CombinedFrame, FrameToShow, CanvasShape, SelectedContour
+class _PolygonLassoToolClass(selectRegionClass._SelectRegion):
+    def __init__(self, Canvas, window_title, AskLayerNames=True, CorrectXYWhileSelecting=True):
+        super().__init__(Canvas, window_title, AskLayerNames=AskLayerNames, CorrectXYWhileSelecting=CorrectXYWhileSelecting)
 
-    # If while selecting the region, mouse goes out of the frame, then clip it position 
-    # to the nearest corner/edge of the frame
-    if selecting:
-        x, y = hf.Correct_xy_While_Selecting(x, y, [0, CanvasShape[1]-1], [0, CanvasShape[0]-1])
-    
-    
-    # Starts selecting - Left button is pressed down
-    if event == cv2.EVENT_FLAG_LBUTTON:
-        if isSelected:
-            isSelected = False
-            selecting = True
-            SelectedContour = []
-            SelectedContour.append([[x, y]])
-        else:
-            if not selecting:
-                selecting = True
-                SelectedContour = []
-                SelectedContour.append([[x, y]])
-            else:
-                SelectedContour.append([[x, y]])
+        self.Selected_Points = []
 
+    # How to draw the region
+    def DrawRegion(self):
+        if self.selecting:
+            for i in range(len(self.Selected_Points)-1):
+                cv2.line(self.FrameToShow, tuple(self.Selected_Points[i][0]), tuple(self.Selected_Points[i+1][0]), (127, 127, 127), 1)
+            cv2.line(self.FrameToShow, tuple(self.Selected_Points[-1][0]), (self.x, self.y), (127, 127, 127), 1)
+        elif self.isSelected:
+            cv2.drawContours(self.FrameToShow, [np.array(self.Selected_Points)], -1, (127, 127, 127), 1)
 
-    # Selecting the region
-    elif event == cv2.EVENT_MOUSEMOVE:
-        if selecting:
-            FrameToShow = CombinedFrame.copy()
-            ContourCopy = SelectedContour.copy()
-            ContourCopy.append([[x, y]])
-            cv2.drawContours(FrameToShow, [np.array(ContourCopy)], -1, (127, 127, 127), 1)
-
-    # Stop selecting the layer.
-    elif event == cv2.EVENT_LBUTTONDBLCLK:
-        isSelected = True
-        selecting = False
-        if len(SelectedContour) <= 2:
-            SelectedContour = []
-            isSelected = False
-        SelectedContour.append([[x, y]])
-        FrameToShow = CombinedFrame.copy()
-        cv2.drawContours(FrameToShow, [np.array(SelectedContour)], -1, (127, 127, 127), 1)
-
-
-def PolygonLassoTool(Canvas, window_title):
-    hf.Clear()
-    # Taking layer numbers user wants to copy
-    Canvas.PrintLayerNames()
-    layer_nos_to_copy = AskLayerNumsToCopy(-1, len(Canvas.layers) - 1)
-
-    print("\nPress 'Y' to confirm selection and copy it in a new layer else press 'N' to abort.")
-    print("You can also used the keys 'W', 'A', 'S', and 'D', to move the")
-    print("selected region Up, Left, Down, and Right respectively.")
-
-    # Clearing mouse buffer data (old mouse data) - this is a bug in OpenCV probably
-    cv2.namedWindow(window_title)
-    cv2.setMouseCallback(window_title, hf.EmptyCallBackFunc)
-    Canvas.Show(Title=window_title)
-    cv2.waitKey(1)
-
-    # Setting mouse callback
-    cv2.setMouseCallback(window_title, CallBackFunc_PolyLassoTool)
-
-    # Setting some params used in callback function
-    global selecting, isSelected, CombinedFrame, FrameToShow, CanvasShape, SelectedContour
-    selecting = False       # True if region is being selected      
-    isSelected = False      # True if region is selected
-    Canvas.CombineLayers()
-    CombinedFrame = Canvas.CombinedImage.copy()     # the combined frame of the canvas
-    FrameToShow = CombinedFrame.copy()              # The frame which will be shown (with the selected region)
-    CanvasShape = Canvas.Shape                      # Shape of the canvas
-    SelectedContour = []                            # Contour of the selected region
-
-
-    IsAborted = False
-    while True:
-        # Showing canvas
-        cv2.imshow(window_title, FrameToShow)
-        Key = cv2.waitKey(1)
-
-        if Key == 89 or Key == 121:         # If 'Y'/'y' - confirm
-            if isSelected:                  # If the region is selected
-                break
-            else:                           # If the region is not selected yet
-                print("Select a region first to confirm.")
-                continue
-        elif Key == 78 or Key == 110:       # If 'N'/'n' - abort
-            IsAborted = True
-            break
+    # Callback function will be fully modified
+    def CallBackFunc(self, event, x, y, flags, params):
+        # If while selecting the region, mouse goes out of the frame, then clip it position 
+        # to the nearest corner/edge of the frame
+        if self.selecting:
+            self.x, self.y = hf.Correct_xy_While_Selecting(self.x, self.y, [0, self.CanvasShape[1]-1], [0, self.CanvasShape[0]-1])
         
-        # If the region is selected, check if the user is trying to move it
-        if isSelected:
-            if Key == 87 or Key == 119:     # If 'W'/'w' - move up
-                SelectedContour = hf.ShiftContour(SelectedContour, ToOrigin=False, ShiftBy=[0, -1])
-            if Key == 65 or Key == 97:      # If 'A'/'a' - move left
-                SelectedContour = hf.ShiftContour(SelectedContour, ToOrigin=False, ShiftBy=[-1, 0])
-            if Key == 83 or Key == 115:     # If 'S'/'s' - move down
-                SelectedContour = hf.ShiftContour(SelectedContour, ToOrigin=False, ShiftBy=[0, 1])
-            if Key == 68 or Key == 100:     # If 'D'/'d' - move right
-                SelectedContour = hf.ShiftContour(SelectedContour, ToOrigin=False, ShiftBy=[1, 0])
+        # Storing mouse pointer values to self variable for access outside the function
+        self.x, self.y = x, y
+        
+        # Starts selecting - Left button is pressed down
+        if event == cv2.EVENT_FLAG_LBUTTON:
+            self.selecting = True
+            if self.isSelected:     # If already selected, start drawing a new one
+                self.isSelected = False
+                self.Selected_Points = []
+            self.Selected_Points.append([[x, y]])
             
-            FrameToShow = CombinedFrame.copy()
-            cv2.drawContours(FrameToShow, [np.array(SelectedContour)], -1, (127, 127, 127), 1)
-            
+        # Selecting the region
+        elif event == cv2.EVENT_MOUSEMOVE:
+            if self.selecting:
+                self.SetCanvasFrame()
 
-    if not IsAborted:
+        # Stop selecting the layer.
+        elif event == cv2.EVENT_LBUTTONDBLCLK:
+            self.isSelected = True
+            self.selecting = False
+            if len(self.Selected_Points) <= 2:
+                self.Selected_Points = []
+                self.isSelected = False
+            else:
+                self.Selected_Points.append([[x, y]])
+            self.SetCanvasFrame()
+
+    # Move when region is selected
+    def Region_isSelected(self):
+        if self.Key == 87 or self.Key == 119:     # If 'W'/'w' - move up
+            self.Selected_Points = hf.ShiftContour(self.Selected_Points, ToOrigin=False, ShiftBy=[0, -1])
+        elif self.Key == 65 or self.Key == 97:      # If 'A'/'a' - move left
+            self.Selected_Points = hf.ShiftContour(self.Selected_Points, ToOrigin=False, ShiftBy=[-1, 0])
+        elif self.Key == 83 or self.Key == 115:     # If 'S'/'s' - move down
+            self.Selected_Points = hf.ShiftContour(self.Selected_Points, ToOrigin=False, ShiftBy=[0, 1])
+        elif self.Key == 68 or self.Key == 100:     # If 'D'/'d' - move right
+            self.Selected_Points = hf.ShiftContour(self.Selected_Points, ToOrigin=False, ShiftBy=[1, 0])
+        
+    # Extracting selected regions BB and Mask
+    def GetSelectedRegionDetails(self):
         # Finding Selected regions bounding box, its contour shifted to origin, and its shifted mask image
         # Selected_BB = list(cv2.boundingRect(np.array(SelectedContour)))
-        SelectedContourToOrigin, Selected_Mask, Selected_BB = hf.ShiftContour(SelectedContour, Get_Mask_BB=True)
+        _, self.Selected_Mask, self.Selected_BB = hf.ShiftContour(self.Selected_Points, Get_Mask_BB=True)
 
         # Redrawing mask to conver any uncovered area
-        OuterContour, _ = cv2.findContours(Selected_Mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-        cv2.drawContours(Selected_Mask, OuterContour, -1, 255, -1)
+        OuterContour, _ = cv2.findContours(self.Selected_Mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        cv2.drawContours(self.Selected_Mask, OuterContour, -1, 255, -1)
+        
 
-        # Extracting the selected regions and copying them in a new layer
-        ExtractSelectedRegion(Canvas, Selected_BB, Selected_Mask, layer_nos_to_copy)
-    
-    else:
-        print("\nRegion selection aborted.")
-
+def PolygonLassoTool(Canvas, window_title):
+    ToolObject = _PolygonLassoToolClass(Canvas, window_title)
+    ToolObject.RunTool()
 
 #######################################################################################################################################################
 
